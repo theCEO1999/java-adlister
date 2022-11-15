6f68c3648ae84b39630d99b2f3431b34f4859dbf
Caleb O'Kane <caleb.ceo.okane@gmail.com>
2022-11-15 11:31:29 -0600

    complete intro to servlets

---
+++ src/main/java/ParameterTestServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name= "ParameterTestServlet", urlPatterns = "/parameterTest")
+public class ParameterTestServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("test/html");
+        PrintWriter out = response.getWriter();
+        String word = request.getParameter("word");
+        System.out.println("<h1>You entered " + word + "!</h1>");
+    }
+}
